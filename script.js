8a205592d68762958cbaa22793429c85aa43845a
GiuliaV <valente.giulia93@gmail.com>
2022-05-10 15:57:50 +0200

    Inizio Progetto

---
+++ script.js
@@ -0,0 +1,18 @@
+
+// Risistemare i Jsnack fatti insieme questa mattina.
+// #crea 2 array, di 5 stringhe il primo e di 2 stringhe il secondo, stampa dentro una UL dove ogni LI ha come
+//  contenuto principale in ordine, le parole del primo array e come sotto-contenuto annidato le 2 parole del secondo array in una UL annidata, tipo:
+// <ul>
+//  	<li>parola1 arr1
+// 		<ul>
+//  			<li>parola1 arr2</li>
+// 			<li>parola2 arr2</li>
+// 		</ul>
+// 	</li>
+// 	<li>parola2 arr1
+// 		<ul>
+//  			<li>parola1 arr2</li>
+// 			<li>parola2 arr2</li>
+// 		</ul>
+// 	</li>
+// 	<li>parola3 arr1
